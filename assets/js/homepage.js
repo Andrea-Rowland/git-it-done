e5e51184ee5b68eb8a8f2361999917cbea00cd5a
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-04-11 15:29:44 -0600

    uncaught in promis type error

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,5 @@
 var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#userName");
+var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
