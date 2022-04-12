9e9a9e0f201dee31e0b23dc48818ab1d60f002d8
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-04-12 14:47:19 -0600

    added fetch function to single.js

---
+++ single.js
@@ -0,0 +1,19 @@
+var getRepoIssues = function(repo) {
+    console.log(repo);
+
+    var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    fetch(apiURL).then(function(response) {
+        //request was successful
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
+getRepoIssues("facebook/react");
